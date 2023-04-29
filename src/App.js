acb6a32367f357602857e4c9fa1031b5264aba60
George Leyva <leyvageorge@hotmail.com>
2023-04-29 12:12:11 -0700

    changes as by professor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 import './App.css';
-import { useSelector, useDispatch, connect } from 'react-redux'
-import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
 import { useEffect } from 'react';
+import { useSelector, useDispatch,connect } from 'react-redux';
+import { 
+  fetchData,
+  nextImage,
+  prevImage,
+  setArtId,
+  reset 
+} from './features/dataSlice';
 
-function App(props) {
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.data)
+const mapStateToProps = state =>{
+  return state.data;
+};
 
-  const renderImg = () => {
-    if(data.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
-    } else {
-      return <p>image here</p>
-    }
-  }
+function App({ artId, isLoggedIn }) {
+  const dispatch = useDispatch();
+  const currentState = useSelector(state => state.data);
 
   useEffect(() => {
-    dispatch(fetchData())
-  }, [props.objectId, dispatch])
+    dispatch(fetchData());
+  }, [artId,dispatch]);
+
+  const renderImage = () => {
+    return currentState.apiData ?
+    <img src={ currentState.apiData.primaryImage } />
+    :
+    <h3>No image available</h3>
+  }
+
+  return(
+    <div className='App' style={{ 'backgroundColor': isLoggedIn ? 'red': 'white'}}>
+      <div className='button-container'>
+        <button onClick={
+          () => dispatch(fetchData())
+        }>Trigger Thunk</button>
+
+        <button onClick={
+          () => dispatch(reset())
+        }>Reset</button>
+
+        <button onClick={
+          () => dispatch(nextImage())
+        }>Next Image</button>
+
+        <button onClick={
+          () => dispatch(prevImage())
+        }>Back</button>
 
 
-  return (
-    <div className="App">
-      <div>
-        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
-        <button onClick={() => dispatch(clearData())}>Clear</button>
-        <button onClick={() => dispatch(incrementId())}>Next</button>
-        <button onClick={() => dispatch(decrementId())}>Back</button>
       </div>
-      <input value={ data.objectId } onChange={(e) => {
-        dispatch(inputId(Number(e.target.value)))
-      }} />
-      <div>
-        {data.objectId}
-        {renderImg()}
+      <div className='input-controls'>
+        <input
+          type='text'
+          value={currentState.artId}
+          onChange={
+            e => dispatch(setArtId(e.target.value))
+          }
+        />
+      </div>
+      <div className='image-container'>
+        {
+          renderImage()
+        }
       </div>
     </div>
   );
 }
 
-
-const mapStateToProps = (state, ownProps) => ({ objectId: state.data.objectId })
-
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -1,42 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit'
+const getApiUrl = artId => `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`
 
 const initialState = {
-    objectId: 10245,
-    apiData: {}
+    objectId: 24576,
+    apiData: {},
+    isLoggedIn: true
 }
 
 export const dataSlice = createSlice({
     name: 'data',
     initialState,
     reducers: {
-        setData: (state, action) => {
-            return {...state, apiData : action.payload}
+        loadData: (state,action) => {
+            state.apiData = action.payload;
         },
-        clearData: () => {
-            return initialState
+        nextImage: (state) => {
+            state.artId++;
         },
-        inputId: (state, action) => {
-            return { ...state, objectId: action.payload }
+        prevImage: (state) => {
+            state.artId--;
         },
-        incrementId: (state) => {
-            return { ...state, objectId: state.objectId + 1 }
+        setArtId: (state, action) => {
+            state.artId = action.payload;
         },
-        decrementId: (state) => {
-            return { ...state, objectId: state.objectId - 1 }
-        }
+        reset: () => {
+            return initialState;
+        },
     }
 })
 
-export const { setData, clearData, incrementId, decrementId, inputId } = dataSlice.actions
+export const { 
+    loadData, 
+    nextImage, 
+    prevImage,
+    setArtId, 
+    reset
+} = dataSlice.actions
 
 export const fetchData = () => {
-    const fetchDataThunk = async (dispatch, getState) => {
-        let state = getState()
-        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${state.data.objectId}`)
-        const rData = await response.json()
-        dispatch(setData(rData))
+    return async (dispacth, getState) => {
+        const data = getState();
+        const { artId } = data.data;
+        const response = await fetch(getApiUrl(artId));
+        const json = await response.json();
+        dispacth(loadData(json))
     }
-    return fetchDataThunk
 }
 
 export default dataSlice.reducer
